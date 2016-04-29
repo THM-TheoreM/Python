5283a528bbc4909882bd5d9e85da8b9bb16419ec
gaoxinge <gaoxx5@gmail.com>
2016-04-29 14:32:23 +0800

    kNN

---
+++ 12 scikit-learn/test.py
@@ -0,0 +1,12 @@
+"""
+===
+kNN
+===
+It is kNN method from http://scikit-learn.org/stable/auto_examples/neighbors/plot_classification.html#example-neighbors-plot-classification-py.
+Sample usage of Nearest Neighbors classification.
+It will plot the decision boundaries for each class.
+
+Because there are many exciting things to learn from the official website http://scikit-learn.org/stable/, we don't place all codes in this document: 12 scikit-learn.
+If you have learned any method based on scikit-learn, you can add codes in this document. 
+"""
+print __doc__
